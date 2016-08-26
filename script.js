0e170240980b83fe21f8cdab9d7b17da871f9b37
advanced bear <advanced.bear@gmail.com>
2016-08-26 19:00:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿function BAN() {
+	if($(".result").css("display") != "none") {
+		$(".result").slideUp("fast");
+	}
+	var text = document.getElementById("input").value;
+	document.getElementById("reason_text").innerHTML = text;
+
+	var dt = new Date();
+	var year = dt.getFullYear();
+	var month = dt.getMonth()+1;
+	var day = dt.getDate();
+	var hours = dt.getHours();
+	var minutes = dt.getMinutes();
+	var date = year + '/' + month + '/' + day +' '+ hours + ':' + minutes + ' ～';
+
+	document.getElementById("date").innerHTML = date;
+
+	if($(".result").css("display") == "none"){
+		$(".result").slideDown("fast");
+	}
+}
